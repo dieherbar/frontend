d1c137e0966b5ee05fa73748de14021694e35918
Diego Barletta <diegohbarletta@gmail.com>
2023-06-27 23:09:34 -0300

    firebase code

---
+++ proyecto001/src/firebase/firebaseCode.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC88WDVNAEqQcvqv1h-gj4FZwFF9Ze3RhI",
+  authDomain: "pruebadiegob.firebaseapp.com",
+  projectId: "pruebadiegob",
+  storageBucket: "pruebadiegob.appspot.com",
+  messagingSenderId: "970836218620",
+  appId: "1:970836218620:web:544807ba67f96813fabd79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
